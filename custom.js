3ac01167485c12556799a4518ed01630c109c692
Martin Viljoen <mailmartinviljoen@gmail.com>
2024-08-13 21:24:14 +0400

    Add files via upload

--- custom.js
+++ custom.js
@@ -19,21 +19,38 @@ xhr.onload = () => {
 };
 
 function FuncDisableAdminMode(){
-const xhr = new XMLHttpRequest();
-xhr.open("GET", "api/enableadmin");
-xhr.send();
-xhr.responseType = "text";
-xhr.onload = () => {
-  if (xhr.readyState == 4 && xhr.status == 200) {
-    console.log(xhr.response);
-	//alert(xhr.response);
-	if(xhr.response=="Admin mode disabled (Note: system is now secure until you enable admin mode."){			
-			document.getElementById("idAdminModestatus").style.visibility = "hidden"; 
-	}
-	
-  } else {
-    console.log(`Error: ${xhr.status}`);
-	
-  }
-};
+	const xhr = new XMLHttpRequest();
+	xhr.open("GET", "api/enableadmin");
+	xhr.send();
+	xhr.responseType = "text";
+	xhr.onload = () => {
+	  if (xhr.readyState == 4 && xhr.status == 200) {
+		console.log(xhr.response);
+		//alert(xhr.response);
+		if(xhr.response=="Admin mode disabled (Note: system is now secure until you enable admin mode."){			
+				document.getElementById("idAdminModestatus").style.visibility = "hidden"; 
+		}
+		
+	  } else {
+		console.log(`Error: ${xhr.status}`);
+		
+	  }
+	};
+}
+function FuncEnableAdminMode{	
+	var parmPassword=document.getElementById("idAdminPassword").value;
+	const xhr = new XMLHttpRequest();
+	xhr.open("GET", "api/enableadmin?adminpass=" + parmPassword);
+	xhr.send();
+	xhr.responseType = "text";
+	xhr.onload = () => {
+	  if (xhr.readyState == 4 && xhr.status == 200) {
+		console.log(xhr.response);
+		alert(xhr.response);								
+	  } else {
+		console.log(`Error: ${xhr.status}`);
+		
+	  }
+	};
+	document.getElementById("idAdminPassword").value="";
 }
