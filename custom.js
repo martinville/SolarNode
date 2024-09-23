363f5d3e8a0772e9581a49315784140c7c238caa
Martin Viljoen <mailmartinviljoen@gmail.com>
2024-09-23 20:29:41 +0530

    Add files via upload

--- custom.js
+++ custom.js
@@ -140,7 +140,7 @@ function FuncGetFileList(){
 	xhr.responseType = "text";
 	xhr.onload = () => {
 	  if (xhr.readyState == 4 && xhr.status == 200) {
-		console.log(xhr.response);
+		//console.log(xhr.response);
 		//alert(xhr.response);
 		document.getElementById("idFiles").innerHTML = xhr.response;
 		
@@ -187,15 +187,20 @@ function FuncSaveHASettings(){
 		var ParmsToSend='fHAIP=' + fHAIP + '&fHAPort=' + fHAPort + '&fHAHTTP=' + fHAHTTP + '&fToken=' + fToken;
 		alert(ParmsToSend);
 		
-		var http = new XMLHttpRequest();
-		var url = '/savehssettings';						
-		http.open('POST', url, true);
-		http.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
-
-		http.onreadystatechange = function() {//Call a function when the state changes.
-			if(http.readyState == 4 && http.status == 200) {
-				alert(http.responseText);
-			}
-		}
-		http.send(ParmsToSend);		
+		const xhr = new XMLHttpRequest();
+		xhr.open("GET", "api/savehssettings");
+		xhr.send(ParmsToSend);
+		xhr.responseType = "text";
+		xhr.onload = () => {
+		  if (xhr.readyState == 4 && xhr.status == 200) {
+			console.log(xhr.response);
+			alert(xhr.response);
+			document.getElementById("idFiles").innerHTML = xhr.response;
+			
+		  } else {
+			console.log(`Error: ${xhr.status}`);
+			
+		  }
+		};
+	
 }
